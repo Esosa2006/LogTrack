a0e224aa08a6f7d90231d46fb92a82b98aef91c7
Esosa Uwuilekhue <esosauwuilekhue@gmail.com>
2025-08-04 13:58:29 +0100

    fix: added annotations for endpoints

--- src/main/java/com/example/LogTrack/controllers/AdminController.java
+++ src/main/java/com/example/LogTrack/controllers/AdminController.java
@@ -1,14 +1,14 @@
 package com.example.LogTrack.controllers;
 
 import com.example.LogTrack.models.dtos.AssignmentDto;
+import com.example.LogTrack.models.entities.Supervisor;
 import com.example.LogTrack.services.AdminService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -20,7 +20,13 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PostMapping("/assign")
     public ResponseEntity<String> assignStudentToSupervisor(@Valid @RequestBody AssignmentDto assignmentDto) {
         return adminService.assignStudentToSupervisor(assignmentDto);
+    }
+
+    @GetMapping("/supervisors")
+    public List<Supervisor> viewAllSupervisors(){
+        return
     }
 }
